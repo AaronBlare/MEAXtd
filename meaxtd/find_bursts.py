e72358d0d3bd031bb14b3659a35b1826542a2897
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2021-05-21 12:52:08 +0300

    refactor

--- meaxtd/find_bursts.py
+++ meaxtd/find_bursts.py
@@ -4,40 +4,40 @@ from intervaltree import IntervalTree
 
 
 def find_spikes(data, method, coefficient, progress_callback):
-    signals = data.stream
-    num_signals = signals.shape[1]
+    num_signals = data.stream.shape[1]
     total_time_in_ms = int(np.ceil(data.time[-1] * 1000))
     data.TSR = np.zeros(int(total_time_in_ms / 50), dtype=int)
     data.TSR_times = np.arange(0, data.time[-1], 0.05)
     for signal_id in range(0, num_signals):
         progress_callback.emit(round(signal_id * 30 / num_signals))
         if method == 'Median':
-            noise_mad = np.median(np.absolute(signals[:, signal_id])) / 0.6745
-            crossings = detect_threshold_crossings(signals[:, signal_id], data.fs, coefficient * noise_mad, 0.002)
+            noise_mad = np.median(np.absolute(data.stream[:, signal_id])) / 0.6745
+            crossings = detect_threshold_crossings(data.stream[:, signal_id], data.fs, coefficient * noise_mad, 0.002)
         elif method == 'RMS':
-            noise_rms = np.sqrt(np.mean(signals[:, signal_id] ** 2))
-            crossings = detect_threshold_crossings(signals[:, signal_id], data.fs, coefficient * noise_rms, 0.002)
+            noise_rms = np.sqrt(np.mean(data.stream[:, signal_id] ** 2))
+            crossings = detect_threshold_crossings(data.stream[:, signal_id], data.fs, coefficient * noise_rms, 0.002)
         elif method == 'std':
-            noise_std = np.std(signals[:, signal_id])
-            crossings = detect_threshold_crossings(signals[:, signal_id], data.fs, coefficient * noise_std, 0.002)
+            noise_std = np.std(data.stream[:, signal_id])
+            crossings = detect_threshold_crossings(data.stream[:, signal_id], data.fs, coefficient * noise_std, 0.002)
 
-        spikes = get_spike_peaks(signals[:, signal_id], data.fs, crossings, 0.002)
-        spikes_ends, spikes_maxima = get_spike_ends(signals[:, signal_id], data.fs, crossings, 0.002)
-        spikes_amplitudes = [signals[:, signal_id][spikes_maxima[spike_id]] - signals[:, signal_id][spikes[spike_id]]
-                             for spike_id in range(0, len(spikes))]
+        spikes = get_spike_peaks(data.stream[:, signal_id], data.fs, crossings, 0.002)
+        spikes_ends, spikes_maxima = get_spike_ends(data.stream[:, signal_id], data.fs, crossings, 0.002)
+        spikes_amplitudes = [
+            data.stream[:, signal_id][spikes_maxima[spike_id]] - data.stream[:, signal_id][spikes[spike_id]]
+            for spike_id in range(0, len(spikes))]
 
         data.spikes[signal_id] = np.asarray(spikes)
         data.spikes_starts[signal_id] = np.asarray(crossings)
         data.spikes_ends[signal_id] = np.asarray(spikes_ends)
         data.spikes_amplitudes[signal_id] = np.asarray(spikes_amplitudes)
 
-        data.spike_stream[signal_id] = np.empty(len(signals[:, signal_id]))
+        data.spike_stream[signal_id] = np.empty(len(data.stream[:, signal_id]))
         data.spike_stream[signal_id][:] = np.nan
         for peak_id in range(0, len(spikes)):
             TSR_index = int(np.ceil(spikes[peak_id] / 500))
             data.TSR[TSR_index - 1] += 1
             for curr_id in range(crossings[peak_id], spikes_ends[peak_id] + 1):
-                data.spike_stream[signal_id][curr_id] = signals[curr_id, signal_id]
+                data.spike_stream[signal_id][curr_id] = data.stream[curr_id, signal_id]
 
 
 def detect_threshold_crossings(signal, fs, threshold, dead_time):
@@ -89,29 +89,27 @@ def get_spike_ends(signal, fs, minima, search_range):
 def find_burstlets(data, spike_method, spike_coeff, burst_window, progress_callback):
     if not data.spikes:
         find_spikes(data, spike_method, spike_coeff, progress_callback)
-    signals = data.stream
-    spikes = data.spikes
-    num_signals = signals.shape[1]
+    num_signals = data.stream.shape[1]
     window = 10 * burst_window  # sampling frequency 0.1 ms
     for signal_id in range(0, num_signals):
         progress_callback.emit(30 + round(signal_id * 30 / num_signals))
         data.burstlets[signal_id] = []
-        num_spikes = len(spikes[signal_id])
+        num_spikes = len(data.spikes[signal_id])
         curr_burstlet = []
         burstlet_amplitude = []
         burstlet_start = []
         burstlet_end = []
         for spike_id in range(0, num_spikes - 1):
-            if spikes[signal_id][spike_id + 1] - spikes[signal_id][spike_id] < window:
-                curr_burstlet.append(spikes[signal_id][spike_id])
+            if data.spikes[signal_id][spike_id + 1] - data.spikes[signal_id][spike_id] < window:
+                curr_burstlet.append(data.spikes[signal_id][spike_id])
             else:
                 if len(set(curr_burstlet)) >= 5:
-                    curr_burstlet.append(spikes[signal_id][spike_id])
+                    curr_burstlet.append(data.spikes[signal_id][spike_id])
                     data.burstlets[signal_id].append(curr_burstlet)
-                    curr_start_id = np.where(spikes[signal_id] == curr_burstlet[0])[0][0]
-                    curr_end_id = np.where(spikes[signal_id] == curr_burstlet[-1])[0][0]
-                    burstlet_amplitude.append(max(signals[:, signal_id][curr_burstlet[0]:curr_burstlet[-1]]) -
-                                              min(signals[:, signal_id][curr_burstlet[0]:curr_burstlet[-1]]))
+                    curr_start_id = np.where(data.spikes[signal_id] == curr_burstlet[0])[0][0]
+                    curr_end_id = np.where(data.spikes[signal_id] == curr_burstlet[-1])[0][0]
+                    burstlet_amplitude.append(max(data.stream[:, signal_id][curr_burstlet[0]:curr_burstlet[-1]]) -
+                                              min(data.stream[:, signal_id][curr_burstlet[0]:curr_burstlet[-1]]))
                     burstlet_start.append(data.spikes_starts[signal_id][curr_start_id])
                     burstlet_end.append(data.spikes_ends[signal_id][curr_end_id])
                 curr_burstlet = []
@@ -119,11 +117,11 @@ def find_burstlets(data, spike_method, spike_coeff, burst_window, progress_callb
         data.burstlets_ends[signal_id] = np.asarray(burstlet_end)
         data.burstlets_amplitudes[signal_id] = np.asarray(burstlet_amplitude)
 
-        data.burstlet_stream[signal_id] = np.empty(len(signals[:, signal_id]))
+        data.burstlet_stream[signal_id] = np.empty(len(data.stream[:, signal_id]))
         data.burstlet_stream[signal_id][:] = np.nan
         for peak_id in range(0, len(data.burstlets[signal_id])):
             for curr_id in range(burstlet_start[peak_id], burstlet_end[peak_id] + 1):
-                data.burstlet_stream[signal_id][curr_id] = signals[curr_id, signal_id]
+                data.burstlet_stream[signal_id][curr_id] = data.stream[curr_id, signal_id]
 
 
 def create_interval_tree(data):
@@ -140,9 +138,8 @@ def create_interval_tree(data):
 def find_bursts(data, spike_method, spike_coeff, burst_window, burst_num_channels, progress_callback):
     if not data.burstlets:
         find_burstlets(data, spike_method, spike_coeff, burst_window, progress_callback)
-    signals = data.stream
-    signal_len = len(signals[:, 0])
-    num_signals = signals.shape[1]
+    signal_len = len(data.stream[:, 0])
+    num_signals = data.stream.shape[1]
     burst_detection_function = np.empty(signal_len, dtype=int)
     burst_detection_function[:] = 0
     for signal_id in range(0, num_signals):
@@ -226,9 +223,9 @@ def find_bursts(data, spike_method, spike_coeff, burst_window, burst_num_channel
 
     for signal_id in range(0, num_signals):
         progress_callback.emit(70 + round(signal_id * 10 / num_signals))
-        data.burst_stream[signal_id] = np.empty(len(signals[:, signal_id]))
+        data.burst_stream[signal_id] = np.empty(len(data.stream[:, signal_id]))
         data.burst_stream[signal_id][:] = np.nan
-        data.burst_borders[signal_id] = np.empty(len(signals[:, signal_id]))
+        data.burst_borders[signal_id] = np.empty(len(data.stream[:, signal_id]))
         data.burst_borders[signal_id][:] = np.nan
         for burst_id in range(0, len(data.bursts_starts[signal_id])):
             curr_start = data.bursts_starts[signal_id][burst_id]
@@ -242,14 +239,13 @@ def find_bursts(data, spike_method, spike_coeff, burst_window, burst_num_channel
             if burst_id in data.bursts_burstlets[signal_id]:
                 for curr_id in range(data.burstlets_starts[signal_id][burst_id],
                                      data.burstlets_ends[signal_id][burst_id]):
-                    data.burst_stream[signal_id][curr_id] = signals[curr_id, signal_id]
+                    data.burst_stream[signal_id][curr_id] = data.stream[curr_id, signal_id]
 
 
 def calculate_characteristics(data, progress_callback):
     progress_callback.emit(80)
 
-    signals = data.stream
-    num_signals = signals.shape[1]
+    num_signals = data.stream.shape[1]
     num_seconds = data.time[-1]
     total_num_spikes = 0
     for signal_id in range(0, num_signals):
@@ -295,10 +291,12 @@ def calculate_characteristics(data, progress_callback):
 def save_tables_to_file(data, filepath, spike_method, spike_coeff, burst_window, burst_num_channels, progress_callback):
     progress_callback.emit(90)
     path = filepath[:-3]
-    os.mkdir(path)
+    if not os.path.isdir(path):
+        os.mkdir(path)
     suffix = f"{spike_method}_{str(spike_coeff)}_{str(burst_window)}_{str(burst_num_channels)}"
     path = f"{path}/characteristics_{suffix}/"
-    os.mkdir(path)
+    if not os.path.isdir(path):
+        os.mkdir(path)
     f = open(path + 'global.txt', 'w')
     f.write('Characteristic\tValue\n')
     for key in data.global_characteristics:
--- meaxtd/stat_plots.py
+++ meaxtd/stat_plots.py
@@ -3,17 +3,16 @@ import numpy as np
 
 
 def raster_plot(data):
-    spikes = data.spikes
     num_signals = data.stream.shape[1]
     num_spikes = 0
     for signal_id in range(0, num_signals):
-        num_spikes += len(spikes[signal_id])
+        num_spikes += len(data.spikes[signal_id])
     nodes = np.empty([num_spikes, 2])
     edges = np.empty([num_spikes, 2], dtype=int)
     g = pg.GraphItem()
     node_id = 0
     for signal_id in range(0, num_signals):
-        curr_spikes = spikes[signal_id]
+        curr_spikes = data.spikes[signal_id]
         for curr_spike in curr_spikes:
             nodes[node_id, 0] = data.time[curr_spike]
             nodes[node_id, 1] = signal_id + 1
